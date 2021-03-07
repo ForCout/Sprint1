5aeda62ad4470a68ce9641dc83e73fc492f70136
forcout <monchoforcout@gmail.com>
2021-03-07 19:15:58 +0100

    Revisión

--- Promises & Callbacks/Nivel 2 y 3.js
+++ Promises & Callbacks/Nivel 2 y 3.js
@@ -21,6 +21,7 @@ let salaries = [
   {
     id: 2,
     salary: 1000,
+    
   },
   {
     id: 3,
@@ -31,47 +32,40 @@ let salaries = [
 //Ejercicio 1 Nivel 2
 
 let getEmpleado = (id) => {
-  let empleado = employees.find(item => item.id == id);
+  let empleado = employees.find((item) => item.id == id);
 
   return new Promise((resolve, reject) => {
     if (empleado != undefined) {
       resolve(empleado);
     } else {
-      reject('Empleado no encontrado');
+      reject("Empleado no encontrado");
     }
-    
-    
   });
 };
-
 
 // Ejercicio 2 Nivel 2
 
 let getSalario = (empleado) => {
-  let salario = salaries.find(item => item.id == empleado.id);
+  let salario = salaries.find((item) => item.id == empleado.id);
   return new Promise((resolve, reject) => {
-    if (salario != undefined) {
+    if (salario.salary != undefined) {
       resolve(salario.salary);
     } else {
-      reject('El salario no ha sido encontrado')
+      reject("El salario no ha sido encontrado");
     }
-   
   });
 };
- 
 
 //ejercicio 3 Nivel 2
 
 getEmpleado(2)
-  .then(empleado => {
-    return getSalario(empleado)
-  }).then(salario => console.log(salario));
+  .then(empleado => getSalario(empleado))
+  .then(salario => console.log(salario));
  
 
-
 // Nivel 3
-getEmpleado(4)
-  .then(empleado => {
-    return getSalario(empleado)
-  }).then(salario => console.log(salario))
-  .catch(error => console.log(error))
+getEmpleado(3)
+  .then(empleado => getSalario(empleado))
+  .then(salario => console.log(salario))
+  .catch(error => console.log(error));
+  
